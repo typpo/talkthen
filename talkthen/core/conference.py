df23e91a6a521099ca7068759c88dbad42881976
Ian Webster <typppo@gmail.com>
2014-01-25 16:05:56 -0800

    work on texting confirmation

--- talkthen/core/conference.py
+++ talkthen/core/conference.py
@@ -1,3 +1,5 @@
+import random
+import string
 import twilio.twiml
 from twilio.rest import TwilioRestClient
 from talkthen.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_CALLER_ID
@@ -9,3 +11,48 @@ def start_call(number, call_pk):
       url='http://www.ianww.com/talkthen/core/call_placed/%s/' % call_pk)
 
   return call.sid
+
+def schedule_call(from_num, to_num):
+  try:
+    from_phone = Phone.objects.get(number=from_num)
+  except:
+    from_phone = Phone(number=from_num)
+    from_phone.save()
+
+  try:
+    to_phone = Phone.objects.get(number=to_num)
+  except:
+    to_phone = Phone(number=to_num)
+    to_phone.save()
+
+  # Ensure nothing outside US
+  if not to_phone.number.startswith('+1') or not from_phone.number.startswith('+1'):
+    return {
+            'success': False,
+            'message': 'We do not currently support numbers outside the US',
+            }
+
+  newcall = Call(owner_number=from_phone)
+  newcall.participant_numbers.add(to_phone)
+  newcall.description = 'placeholder desc'
+  newcall.scheduled_for = datetime.datetime.now()
+  newcall.confirmation_code = generate_confirmation_code()
+
+  newcall.save()
+
+  # Send confirmation text
+  client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+  confirm_msg = 'Confirm your call with %s by responding with: %s' % \
+          (from_phone.number, newcall.confirmation_code)
+  client.message.create(to=to_phone.number, from_=TWILIO_CALLER_ID,
+          body=confirm_msg)
+
+  return {
+          'success': True,
+          }
+
+def generate_confirmation_code():
+  return random_char(5).lower()
+
+def random_char(y):
+  return ''.join(random.choice(string.ascii_letters) for x in range(y))
--- talkthen/core/views.py
+++ talkthen/core/views.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+import json
 
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
@@ -9,34 +10,12 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect
 import twilio.twiml
 from core.models import Call, PhoneNumber
 
+import conference
+
 def create(request, from_num, to_num):
-  # TODO ensure nothing outside of the US
 
-  # Convert to standard number format
-  from_num = PhoneNumber.convert_to_e164(from_num)
-  to_num = PhoneNumber.convert_to_e164(to_num)
-
-  # Create a call from
-  try:
-    from_phone = Phone.objects.get(number=from_num)
-  except:
-    from_phone = Phone(number=from_num)
-    from_phone.save()
-
-  try:
-    to_phone = Phone.objects.get(number=to_num)
-  except:
-    to_phone = Phone(number=to_num)
-    to_phone.save()
-
-  newcall = Call(owner_number=from_phone)
-  newcall.participant_numbers.add(to_phone)
-  newcall.description = 'placeholder desc'
-  newcall.scheduled_for = datetime.datetime.now()
-
-  newcall.save()
-
-  return ''
+  resp = conference.schedule_call(from_num, to_num)
+  return HttpResponse(json.dumps(resp))
 
 
 @require_http_methods(['GET', 'POST'])
@@ -62,3 +41,22 @@ def call_placed(request, call_pk):
   resp.say('The call has ended.  You may hang up now.')
 
   return HttpResponse(str(resp))
+
+@require_http_methods(['GET', 'POST'])
+@csrf_exempt
+def text_confirmation_reply(request):
+  from_number = request.REQUEST.get('From', None)
+  msg = request.REQUEST.get('Body', None)
+
+  print 'Got MSG from %s: %s' % (from_number, msg)
+
+  # TODO
+  resp = twilio.twiml.Response()
+  resp.message(message)
+
+  return HttpResponse(str(resp))
+
+
+def associate_email():
+  # Record email under a phone number
+  pass
